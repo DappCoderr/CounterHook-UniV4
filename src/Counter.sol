90c2cdecbf4c27b933fb19ea32e108052ad4d7fc
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-06-06 18:58:02 +0530

    remappings, counter.sol

---
+++ src/Counter.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {BaseHook} from "v4-periphery/src/utils/BaseHook.sol";
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+import {Hooks} from "v4-core/libraries/Hooks.sol";
+
+contract CounterHook is BaseHook{ 
+
+    constructor(IPoolManager _manager) BaseHook(_manager){}
+
+    function getHookPermissions() public pure override returns (Hooks.Permissions memory){
+        return Hooks.Permissions({
+            beforeInitialize: false,
+            afterInitialize: false,
+            beforeAddLiquidity: false,
+            afterAddLiquidity: false,
+            beforeRemoveLiquidity: false,
+            afterRemoveLiquidity: false,
+            beforeSwap: false,
+            afterSwap: false,
+            beforeDonate: false,
+            afterDonate: false,
+            beforeSwapReturnDelta: false,
+            afterSwapReturnDelta: false,
+            afterAddLiquidityReturnDelta: false,
+            afterRemoveLiquidityReturnDelta: false
+        });
+    }
+
+}
+
